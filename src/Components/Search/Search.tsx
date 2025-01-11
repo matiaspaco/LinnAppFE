cde5438eea152e9f31b605c5e513515b0311f3b3
matiaspaco <matiaspacoviscarra@gmail.com>
2025-01-11 03:29:51 +0000

    feature: mouse click event added.

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent } from 'react'
 
 type Props = {}
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
+
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  }
+  // const onClick = (e: MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
+  
   return (
-    <div>Search</div>
+    <div>
+      <input value={search} onChange={(e) => handleChange(e)}></input>
+      <button onClick={(e) => onClick(e)}/>
+    </div>
   )
 }
 
